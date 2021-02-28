26a6298f0f8d3ca03c8f122f4f26330a6aa7cbe9
Roben Costa <robenncosta@gmail.com>
2021-02-28 00:57:28 -0300

    feat: initial commit of project

---
+++ src/main/java/com/robencosta/bookcatalogservice/BookCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.robencosta.bookcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogServiceApplication.class, args);
+	}
+}
